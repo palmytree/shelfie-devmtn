21f4688448bd0890ee325fec6872b5c845782266
palmytree <jeff@palmytree.com>
2020-05-29 17:48:50 -0500

    added responsiveness

--- server/index.js
+++ server/index.js
@@ -3,17 +3,19 @@ require('dotenv').config()
 const express = require('express'),
 	massive = require('massive'),
 	ctrl = require('./controller'),
+	path = require('path'),
 	app = express(),
 	{ SERVER_PORT, CONNECTION_STRING } = process.env
 
 app.use(express.json())
+app.use('/', express.static(path.join(__dirname, '../build')))
 
 massive({
 	connectionString: CONNECTION_STRING,
 	ssl: {
 		rejectUnauthorized: false
 	}
-})
+	})
 	.then(db => {
 		app.set('db', db)
 		console.log('DB connected')
